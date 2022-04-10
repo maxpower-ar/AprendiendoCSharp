f1fbe81196656a78a49977f6848e66361b71293a
gonzalorf@hotmail.com <gonzalorf@hotmail.com>
2022-04-10 15:30:40 -0300

    Console WriteLine, ReadLine, variables numéricas y string. Suma de números, concatenación de cadenas.

---
+++ Dia1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Este es mi primer programa 
+
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Mi nombre es Max, cual es el tuyo?");
+//var NombreDePersona = Console.ReadLine();
+//Console.WriteLine("Hola " + NombreDePersona);
+
+
+//Sumar numeros
+
+//var PrimerNumero = 8;
+//var SegundoNumero = 10;
+//var TercerNumero =  "NUEVE";
+
+//var Suma = PrimerNumero + SegundoNumero + TercerNumero + TercerNumero;
+//PrimerNumero = 7;
+//Console.WriteLine("La suma es...." + Suma);
+
+
+//Hacer una calculadora que sume dos numeros;
+Console.WriteLine("Dame un numero");
+var NumeroUno = Console.ReadLine();
+Console.WriteLine("Dame otro numero");
+var NumeroDos = Console.ReadLine();
+
+var Suma = Convert.ToInt32(NumeroUno) + Convert.ToInt32(NumeroDos);
+
+Console.WriteLine("La suma de esos dos numeros es..... " + Suma);
+
+
+
